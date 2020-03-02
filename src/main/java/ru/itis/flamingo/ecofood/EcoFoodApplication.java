5c4b92dc27b3fe45c145af13cb00db31ff6cd89b
Anna Kuzmenko <anna.kuzmenko@simbirsoft.com>
2020-03-02 23:19:37 +0300

    project init

---
+++ src/main/java/ru/itis/flamingo/ecofood/EcoFoodApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.flamingo.ecofood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoFoodApplication.class, args);
+    }
+
+}
