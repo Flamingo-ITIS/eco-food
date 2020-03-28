10740e6f913fdad51b01afe3b54bbd270a4a143b
Anna Kuzmenko <anna.kuzmenko@simbirsoft.com>
2020-03-29 01:12:19 +0300

    added auth header for swagger

--- src/main/java/ru/itis/flamingo/ecofood/config/Swagger2Config.java
+++ src/main/java/ru/itis/flamingo/ecofood/config/Swagger2Config.java
@@ -1,12 +1,22 @@
 package ru.itis.flamingo.ecofood.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 
 @Configuration
@@ -14,9 +24,38 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swagger2Config {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
             .apis(RequestHandlerSelectors.basePackage("ru.itis.flamingo.ecofood.controller"))
             .paths(PathSelectors.regex("/.*"))
-            .build();
+            .build()
+            .securitySchemes(Lists.newArrayList(apiKey()))
+            .securityContexts(Collections.singletonList(securityContext()));
+    }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder().scopeSeparator(",")
+            .additionalQueryStringParams(null)
+            .useBasicAuthenticationWithAccessCodeGrant(false).build();
+    }
+
+
+    private ApiKey apiKey() {
+        return new ApiKey("apiKey", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+            .forPaths(PathSelectors.any()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+            "global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("apiKey",
+            authorizationScopes));
     }
 }
