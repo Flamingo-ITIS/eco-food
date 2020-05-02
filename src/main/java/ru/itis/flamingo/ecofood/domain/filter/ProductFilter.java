80b90c242994ea728f0e88961e01e3ad71adc251
greenmapc <greenmapc@gmail.com>
2020-05-02 21:50:54 +0300

    added filter by category name

---
+++ src/main/java/ru/itis/flamingo/ecofood/domain/filter/ProductFilter.java
@@ -0,0 +1,15 @@
+package ru.itis.flamingo.ecofood.domain.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import ru.itis.flamingo.ecofood.domain.entity.enums.CategoryName;
+
+@Data
+@AllArgsConstructor
+@Accessors(chain = true)
+public class ProductFilter {
+//    private Boolean costSort;
+//    private Boolean ratingSort;
+    private CategoryName category;
+}
