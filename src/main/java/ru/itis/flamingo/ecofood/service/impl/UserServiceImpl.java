8ce79cac598e2407078c7a7a39611de062477f3d
greenmapc <greenmapc@gmail.com>
2020-05-11 23:05:04 +0300

    updated user update method

--- src/main/java/ru/itis/flamingo/ecofood/service/impl/UserServiceImpl.java
+++ src/main/java/ru/itis/flamingo/ecofood/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package ru.itis.flamingo.ecofood.service.impl;
 
+import liquibase.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,13 +34,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void update(UserDto updatedUser) {
-        if (!(updatedUser.getContactPhone().isEmpty() ||
-                updatedUser.getName().isEmpty() ||
-                updatedUser.getEmail().isEmpty()))
-            userRepository.save(userMapper.mapToEntity(updatedUser));
-        else {
-            throw new IllegalArgumentException("User's data is empty");
-        }
+        userRepository.save(userMapper.mapToEntity(updatedUser));
     }
 
     @Override
